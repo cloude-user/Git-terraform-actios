49d5e075c072a09c3faf24b4302f3c42b23bd22f
Sundeep <sundeep@Z00BE-436F-412C.ww.ad.ba.com>
2025-01-25 17:15:23 +0000

    specified the exact path name

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,12 +1,26 @@
 
-# Lambda Function
+# # Lambda Function
+# resource "aws_lambda_function" "tony" {
+#   function_name    = "siuuu"
+#   runtime          = "python3.9"
+#   role             = aws_iam_role.cloud_execution_role.arn
+#   handler          = "lambda.handler"
+#   s3_bucket        = "sundeep43-lambda-storage001"
+#   s3_key           = "src/lambdas/lambda-package.zip"
+#   environment {
+#     variables = {
+#       ENV_VAR1 = "Siuu"
+#       ENV_VAR2 = "Siuuuuu"
+#     }
+#   }
+# }
+
 resource "aws_lambda_function" "tony" {
   function_name    = "siuuu"
   runtime          = "python3.9"
   role             = aws_iam_role.cloud_execution_role.arn
   handler          = "lambda.handler"
-  s3_bucket        = "sundeep43-lambda-storage001"
-  s3_key           = "src/lambdas/lambda-package.zip"
+  filename         = "src/lambdas/lambda-package.zip"
   environment {
     variables = {
       ENV_VAR1 = "Siuu"
@@ -14,3 +28,4 @@ resource "aws_lambda_function" "tony" {
     }
   }
 }
+
