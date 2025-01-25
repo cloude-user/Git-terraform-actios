5533a05d1ae4390da13ce7dce78727b9940dd85f
Sundeep <sundeep@Z00BE-436F-412C.ww.ad.ba.com>
2025-01-25 16:34:17 +0000

    added s3 bucket and path to the zip

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,6 +5,8 @@ resource "aws_lambda_function" "example_lambda" {
   runtime          = "python3.9"
   role             = aws_iam_role.lambda_execution_role.arn
   handler          = "lambda_function.lambda_handler"
+  s3_bucket        = "sundeep43-lambda-storage001"
+  s3_key           = "terraform/lambda.zip"
   environment {
     variables = {
       ENV_VAR1 = "value1"
