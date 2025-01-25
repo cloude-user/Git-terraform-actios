ecaedaf474c52ac56caa957ba62049a42c871c71
Sundeep <sundeep@Z00BE-436F-412C.ww.ad.ba.com>
2025-01-25 16:47:47 +0000

    Siuu this should work

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 # IAM role for Lambda execution
 resource "aws_iam_role" "cloud_execution_role" {
-  name = "lambda_execution_role"
+  name = "cloud_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
