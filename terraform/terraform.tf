352a2d7c9e41a23eb327920e7c975d11c8429ddd
Sundeep <sundeep@Z00BE-436F-412C.ww.ad.ba.com>
2025-01-25 17:07:37 +0000

    changed the config

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -12,15 +12,3 @@ provider "aws" {
   region = var.region
 }
 
-
-
-
-# # Lambda function infrastructure
-# resource "aws_lambda_function" "lambda_function" {
-#   function_name = "sample_lambda_function"
-#   role          = aws_iam_role.lambda_execution_role.arn
-#   handler       = "index.handler"
-#   runtime       = "python3.9"
-#   s3_bucket     = aws_s3_bucket.lambda_code_bucket.id
-#   s3_key        = "backend.zip"
-# }
